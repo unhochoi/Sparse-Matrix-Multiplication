2bfcfc58b029b2a5b8bc2afcfa51e5783a17590b
unhochoi <yms04089@kookmin.ac.kr>
2021-08-10 16:35:58 +0900

    experiment compare of doe and random

--- scenario-extraction/dataset/for-comparison-doe-and-random/doe/extract-doe-train.R
+++ scenario-extraction/dataset/for-comparison-doe-and-random/doe/extract-doe-train.R
@@ -1,12 +1,15 @@
+input = 0
+output = 0
+
 # AlgDesing이 설치 되어 있지 않다면 다음 명령어를 통해 설치해야한다.
 # install.packages("AlgDesign")
 library(AlgDesign)
 
 # 최적화 데이터를 뽑고자 하는 입력 데이터
-input = read.csv("/Users/bdlab/Desktop/sparse-matrix-multiplication/scenario-extraction/dataset/for-performance-comparison-of-doe-and-random/doe-train-after-spmm/doe-1293.csv")
+input = read.csv("/Users/bdlab/Desktop/sparse-matrix-multiplication/scenario-extraction/dataset/for-comparison-doe-and-random/doe/doe-train-1000.csv")
 
 # data : 입력 데이터 , nTrials : 뽑을 데이터 개수, nRepeats : 전체 프로세스 반복 횟수, criterion : 사용할 최적화 방법
-output = optFederov(data=input, nTrials=1000, nRepeats=20,criterion = "D")
+output = optFederov(data=input, nTrials=800, nRepeats=10,criterion = "D")
 
 # 전체 결과
 output
@@ -20,8 +23,8 @@ options(max.print=1000000)
 # 추출된 행렬의 행 번호
 output$rows
 
-setwd("/Users/bdlab/Desktop/sparse-matrix-multiplication/scenario-extraction/dataset/for-performance-comparison-of-doe-and-random/doe-train-after-spmm/")
+setwd("/Users/bdlab/Desktop/sparse-matrix-multiplication/scenario-extraction/dataset/for-comparison-doe-and-random/doe/")
 
 # 추출된 행렬 csv로 저장, 작업 경로 변경 필요시 setwd("경로")로 변경
-write.csv(output$design, file="doe-train-1000.csv", row.names=FALSE) # row.names : 행 포함 여부
+write.csv(output$design, file="doe-train-800.csv", row.names=FALSE) # row.names : 행 포함 여부
 
